d064bd5c517ba6c308d35cd5bd1a378be0c43d11
kikuchy <kikuchy.h@gmail.com>
2014-01-11 15:06:09 +0900

    add files for Travice CI

---
+++ .travis_build.sh
@@ -0,0 +1,10 @@
+echo "Compiling unit tests..."
+clang -c set.c
+clang++ -lgtest -std=c++11 -lpthread -o unit_test.o -c test.cc
+clang++ -lgtest -std=c++11 -lpthread -o unit_test unit_test.o set.o
+echo "Running unit tests..."
+./unit_test -v
+result=$?
+rm -r unit_test set.o unit_test.o
+echo "Unit tests completed : $result"
+exit $result
